3adbcf6e6c4c23d89fb5518b533ad1e779be357c
mystico53 <konkaiser@gmail.com>
2024-10-03 16:04:15 -0700

    made pub sub and processtext work on prod

--- webapp/functions/index.js
+++ webapp/functions/index.js
@@ -2,11 +2,15 @@ const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 // index.js
-const { processText } = require('./processText');
+//const { processText } = require('./processText');
 const { match } = require('./match');
 const { publishJobText } = require('./publishJobText');
 
+const { processPubSubText } = require('./processPubSubText');
+
 // Export all Cloud Functions
-exports.processText = processText;
+//exports.processText = processText;
 exports.match = match;
 exports.publishJobText = publishJobText;
+
+exports.processText = processPubSubText;
