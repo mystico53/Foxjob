bb5ea12a8981a3a35b607640ee8c9b5f8b403c21
mystico53 <konkaiser@gmail.com>
2024-11-18 22:15:00 -0800

    made native modal work

---
+++ webapp/src/lib/admincomponents/TestModal.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let parent;
+</script>
+
+<div class="bg-red-500 text-white p-8 rounded-lg shadow-lg" style="z-index: 1000;">
+    <h2 class="text-2xl font-bold">Test Modal Content</h2>
+    <p class="mt-4">If you can see this, the component modal is working!</p>
+</div>
