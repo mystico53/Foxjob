2334fea5586071963768d843df62330676df3ac5
mystico53 <konkaiser@gmail.com>
2024-11-19 23:21:04 -0800

    added fixes from zhong

--- webapp/src/routes/landing/+page.svelte
+++ webapp/src/routes/landing/+page.svelte
@@ -4,6 +4,7 @@
     import Features from '$lib/landing/ValueDistinguisher.svelte';
     import Testimonials from '$lib/landing/HowItWorks.svelte';
     import CallToAction from '$lib/landing/FinalCTA.svelte';
+    import Footer from '$lib/landing/Footer.svelte';
   </script>
   
   <div class="container mx-auto px-4 max-w-6xl">
@@ -12,12 +13,5 @@
     <Features />
     <Testimonials />
     <CallToAction />
+    <Footer />
   </div>
-
-  <footer>
-	<div class="container">
-		<a class="privacy" href="/privacy" data-sveltekit-preload-data="off">
-			Privacy Policy
-		</a>
-	</div>
-</footer>
