9548666fdd97c418e4d9511b1a75e16f4bd72ac6
mystico53 <konkaiser@gmail.com>
2024-11-21 14:31:11 -0800

    created userstatestore and checking for resume

---
+++ webapp/src/lib/onboarding/OnboardingProgress.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { userStateStore } from '../../stores/userStateStore';
+</script>
+
+<div class="p-4 text-center">
+	{#if $userStateStore.resume.isUploaded}
+		<p class="text-success-500">Resume uploaded ✓</p>
+	{:else}
+		<p class="text-error-500">Resume not uploaded</p>
+	{/if}
+</div>
