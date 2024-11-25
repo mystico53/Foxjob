869d1ab7bf1e93c54232da6574795314f9867466
mystico53 <konkaiser@gmail.com>
2024-11-25 15:38:23 -0800

    made offscreen work with prod

--- chromeextension/offscreen.js
+++ chromeextension/offscreen.js
@@ -1,44 +1,42 @@
+import { getAuthUrl } from './config.js';
 
-    // This URL must point to the public site
-    //const _URL = 'https://jobille-45494.web.app/auth/signin';
-    const _URL = 'https://foxjob-prod.web.app/auth/signin';
-    const iframe = document.createElement('iframe');
-    iframe.src = _URL;
-    document.documentElement.appendChild(iframe);
-    chrome.runtime.onMessage.addListener(handleChromeMessages);
+// Initialize URL at the top
+let _URL;
+async function initializeAuthUrl() {
+  _URL = await getAuthUrl();
+  const iframe = document.createElement('iframe');
+  iframe.src = _URL;
+  document.documentElement.appendChild(iframe);
+}
 
-    function handleChromeMessages(message, sender, sendResponse) {
-      // Extensions may have an number of other reasons to send messages, so you
-      // should filter out any that are not meant for the offscreen document.
-      if (message.target !== 'offscreen') {
-        return false;
+// Initialize immediately
+initializeAuthUrl();
+
+chrome.runtime.onMessage.addListener(handleChromeMessages);
+
+function handleChromeMessages(message, sender, sendResponse) {
+  if (message.target !== 'offscreen') {
+    return false;
+  }
+
+  function handleIframeMessage({data}) {
+    try {
+      if (data.startsWith('!_{')) {
+        return;
       }
-
-      function handleIframeMessage({data}) {
-        try {
-          if (data.startsWith('!_{')) {
-            // Other parts of the Firebase library send messages using postMessage.
-            // You don't care about them in this context, so return early.
-            return;
-          }
-          data = JSON.parse(data);
-          self.removeEventListener('message', handleIframeMessage);
-
-          sendResponse(data);
-        } catch (e) {
-          console.log(`json parse failed - ${e.message}`);
-        }
-      }
-
-      globalThis.addEventListener('message', handleIframeMessage, false);
-
-      // Initialize the authentication flow in the iframed document. You must set the
-      // second argument (targetOrigin) of the message in order for it to be successfully
-      // delivered.
-      setTimeout(() => {
-        console.log('Sending initAuth message after delay');
-        iframe.contentWindow.postMessage({ initAuth: true }, new URL(_URL).origin);
-      }, 500); 
-      return true;
+      data = JSON.parse(data);
+      self.removeEventListener('message', handleIframeMessage);
+      sendResponse(data);
+    } catch (e) {
+      console.log(`json parse failed - ${e.message}`);
     }
-    
+  }
+
+  globalThis.addEventListener('message', handleIframeMessage, false);
+
+  setTimeout(() => {
+    console.log('Sending initAuth message after delay');
+    iframe.contentWindow.postMessage({ initAuth: true }, new URL(_URL).origin);
+  }, 500);
+  return true;
+}
