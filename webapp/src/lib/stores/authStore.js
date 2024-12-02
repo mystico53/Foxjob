b295482f798550e9653cba3bd07940bfbc058c2c
mystico53 <konkaiser@gmail.com>
2024-12-01 21:10:00 -0800

    added user details to firestore

--- webapp/src/lib/stores/authStore.js
+++ webapp/src/lib/stores/authStore.js
@@ -1,13 +1,23 @@
+// $lib/stores/authStore.js
 import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
-import { auth } from '$lib/firebase';
+import { auth, db } from '$lib/firebase';
 import { onAuthStateChanged } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
 
 function createAuthStore() {
     const { subscribe, set } = writable(null);
 
     if (browser) {
-        onAuthStateChanged(auth, (user) => {
+        onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                // Write basic user info to Firestore
+                const userRef = doc(db, 'users', user.uid);
+                await setDoc(userRef, {
+                    displayName: user.displayName,
+                    email: user.email
+                }, { merge: true }); // merge: true prevents overwriting other fields
+            }
             set(user);
         });
     }
